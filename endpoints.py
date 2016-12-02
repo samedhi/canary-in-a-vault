8165cdfd49d815a17a554e5977c5c99b44503828
Stephen Cagle <stephen@read-line.com>
2016-12-02 13:02:00 -0800

    Got an endpoint that enques '/beat' task

---
+++ endpoints.py
@@ -0,0 +1,31 @@
+from flask import request
+from google.appengine.api import taskqueue
+from vault import client, full_reload, renew_token
+import logging
+
+@app.route('/vault/refresh')
+def vault_refresh():
+    """
+    Periodically called by cron.yaml
+
+    This enqueues roughly one task a second that attempts to
+    read from a location in Vault. Look in your GAE logs to
+    confirm that you were/are able to read from Vault.
+    """
+
+    # There is a good deal of overlap here. This is done so that
+    # we can be fairly certain that every second had one task
+    # enqueued for it. I want a regular (no skipping) heartbeat.
+    for i in range(120):
+        # Right now
+        d = datetime.now()
+        # Right now + i seconds in the future
+        d = datetime.timedelta(seconds=i)
+        # Truncated to the second (so each second has unique task)
+        d = d.replace(microsecond=0)
+        taskqueue.add(url='/beat',
+                      name=d.isoformat().replace(':', '_'),
+                      eta=d,
+                      target='heartbeat')
+
+    return "SUCCESS", 200
